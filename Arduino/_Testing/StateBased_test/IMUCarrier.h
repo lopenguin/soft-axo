aa98a4f9052154f8957c7f99d4c217403fb57ebf
lopenguin <lorenzo@caltech.edu>
2021-09-15 19:35:33 -0700

    Added basic state-based control thing

---
+++ Arduino/_Testing/StateBased_test/IMUCarrier.h
@@ -0,0 +1,66 @@
+/*
+A class to read, store, and manipulate IMU data.
+
+Lorenzo Shaikewitz, 8/26/2021
+*/
+#ifndef IMUCARRIER_H
+#define IMUCARRIER_H
+
+#include "constants.h"
+
+#include <NXPMotionSense_Lorenzo.h>
+#include "utility/NXPSensorRegisters.h"
+#include <Wire.h>
+#include <EEPROM.h>
+
+#include <SerialFlash.h>
+#include <SPI.h>
+
+
+class IMUCarrier {
+public:
+    IMUCarrier(uint8_t in_FXOS8700_addr = FXOS8700_I2C_ADDR0, uint8_t in_FXAS21002_addr = FXAS21002_I2C_ADDR0)
+               : m_imu(in_FXOS8700_addr, in_FXAS21002_addr), m_filter{}
+    {/*does nothing*/}
+
+    bool begin() {
+        m_filter.begin(property::IMU_UPDATE_HZ);
+        return m_imu.begin();
+    }
+
+    bool available() { return m_imu.available(); }
+
+    void update() {
+        m_imu.readMotionSensor(m_ax, m_ay, m_az, m_gx, m_gy, m_gz, m_mx, m_my, m_mz);
+        m_filter.update(m_gx, m_gy, m_gz, m_ax, m_ay, m_az, m_mx, m_my, m_mz);
+        m_filter.getQuaternion(m_quat);
+    }
+
+    const float* getQuat() const { return m_quat; }
+
+    // prints out the quaternion.
+    const void printQuat() const;
+
+    // calculates relative quaternion using formula:
+    // R = A^t * B, where R, A, B are rotation matricies
+    // see http://www.songho.ca/opengl/gl_quaternion.html for the transform
+    // and https://www.euclideanspace.com/maths/geometry/rotations/conversions/matrixToQuaternion/
+    // to convert back to quats
+    void getRelQuatWithRotMatrix(IMUCarrier& q2, float* relQuat) const;
+
+    // well. Math suggests quaternion left multiplication does same thing!
+    // note: may be unsave to use. Sometimes causes random, unexplainable stop.
+    void getRelQuat(IMUCarrier& q2, float* relQuat) const;
+
+    NXPMotionSense m_imu;
+    NXPSensorFusion m_filter;
+
+private:
+    float m_ax, m_ay, m_az;
+    float m_gx, m_gy, m_gz;
+    float m_mx, m_my, m_mz;
+    float m_quat[4]{};
+};
+
+
+#endif
---
+++ Arduino/_Testing/StateBased_test/StateBased_test.ino
@@ -0,0 +1,132 @@
+/*
+Tests operation of ankle exo using state-based control, informed by IMU angle
+readings at leg and foot.
+
+Lorenzo Shaikewitz, 9/15/2021
+*/
+// Reading IMU
+#include <NXPMotionSense_Lorenzo.h>
+#include "utility/NXPSensorRegisters.h"
+#include <Wire.h>
+#include <EEPROM.h>
+
+// Reading/writing flash
+#include <SerialFlash.h>
+#include <SPI.h>
+
+#include "Axo.h"
+#include "constants.h"
+
+
+// Variables for user
+String FILE_NAME{"walk"};
+const int runTimeSeconds{60};
+
+
+// timekeeping
+unsigned long startTime{};
+float gaitPercent{ 0 };
+
+// function declaration
+void writeMotor(int speed, bool coast = false);
+
+
+void setup() {
+    Serial.begin(9600);
+    pinMode(LED_BUILTIN, OUTPUT);
+    digitalWrite(LED_BUILTIN, HIGH);
+
+    // wait for button press (or serial opening)
+    pinMode(BUTTON_PIN, INPUT_PULLUP);
+    while ((digitalRead(BUTTON_PIN) == HIGH) && !Serial) {
+        delay(50);
+    }
+
+    // connect button as interrupt for abort operation
+    attachInterrupt(digitalPinToInterrupt(pin::JOY_BUTTON), abort, RISING);
+
+    Message m = axo.begin(FILE_NAME);
+    switch (m) {
+        case Message::OK:
+            Serial.println("Axo started successfully!");
+            break;
+        case Message::NO_FLASH_CHIP:
+            Serial.println("Flash chip not detected!");
+            while (1);
+            break;
+        case Message::NO_FLASH_SPACE:
+            Serial.println("No flash space left! Please erase chip.");
+            while (1);
+            break;
+        case Message::FILE_TOO_LONG:
+            Serial.println("File name is too long. Check constants.h for max length.");
+            while (1);
+            break;
+    }
+
+    Serial.print("Saving data to: ");
+    Serial.println(axo.getSavefile());
+    Serial.println("-----");
+
+
+    startTime = micros();
+}
+
+
+void loop() {
+    // generate a quat
+    if (axo.propUpdated()) {
+        if (axo.adaIMUAvail()) {
+            // pretty consistently get ~10250 us per sample!
+            // Serial.println(micros() - lastTime);
+            lastTime = micros();
+            axo.updateAdaIMU();
+        }
+    } else {
+        if (axo.propIMUAvail()) {
+            axo.updatePropIMU();
+            // axo.printData();
+            unsigned long currentTime{micros()};
+            if (currentTime - lastTime > 10000) {
+                // it's like there's an optimal delay here - not sure why
+                // or how to get it, but this combo works.
+
+                delayMicroseconds(300);
+                // check time since start just to be sure we are always saving data
+                if (currentTime - startTime > 5000000) {
+                    if (!axo.saveData()) {
+                        Serial.println("File space exceeded.");
+                        digitalWrite(LED_BUILTIN, HIGH);
+                        while (1) {
+                            blink(LED_BUILTIN);
+                        }
+                    }
+                }
+            } else {
+                if (!axo.saveData()) {
+                    Serial.println("File space exceeded.");
+                    digitalWrite(LED_BUILTIN, HIGH);
+                    while (1) {
+                        blink(LED_BUILTIN);
+                    }
+                }
+                // Serial.println("saving...");
+                // axo.printData();
+                // axo.printRelQuat();
+            }
+
+
+            // work with the relative quaternion
+            axo.
+
+        }
+    }
+}
+
+
+void blink(int pin) {
+    digitalWrite(pin, HIGH);
+    delay(1000);
+    digitalWrite(pin, LOW);
+    delay(1000);
+}
