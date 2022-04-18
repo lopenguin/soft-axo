e89822e44e00ff8112de93895c520ced0a03fc90
Neil Janwani <72714187+njanwani@users.noreply.github.com>
2022-04-17 20:02:21 -0700

    Updated safecontrol

--- Arduino/_Testing/Motors/SafeControl_test/README.txt
+++ Arduino/_Testing/Motors/SafeControl_test/README.txt
@@ -1,4 +1,4 @@
-Here is the desmos link for visualization!
+Here is the desmos link for visualization! It is currently tuned for no-load operation.
 
-https://www.desmos.com/calculator/tzlinx1jd1
-
+https://www.desmos.com/calculator/jkcemlkn5t
+(don't worry about losing the original constants, I have the graph saved to my Desmos profile)
--- Arduino/_Testing/Motors/SafeControl_test/SafeControl_test.ino
+++ Arduino/_Testing/Motors/SafeControl_test/SafeControl_test.ino
@@ -1,79 +1,126 @@
+/*
+ * Sketch to attempt safer control of BLDC motors on ankle exoskeleton. Make note
+ * to switch directionality of motor if bugs arise.
+ * 
+ * Amber Lab 4/17/2022, Lorenzo and Neil
+*/
+
 #include <Servo.h>
-#include <axStack.h>
 
+
+// CONSTANTS-------------------------------------------------------------------
 const int MOTOR_PIN = 4;
+const int ENCODER_PIN = A8;
 
+// ESC CONSTANTS
 const int CALIBRATION_PWM = 1500;
 const int CALIBRATION_TIME = 7000;
-const int CONTROL_RATE = 100; // in HZ
-const double C = 0.005;
-const double E = 2.71828;
-const double K_p = 0.7;
+const int CONTROL_RATE = 100;       // in HZ (approximate, will likely be slower)
+
+// POTENTIOMETER CONSTANTS
+const int POT_ZERO = 0;             // lowest potentiometer value achievable
+const int POT_MAX = 1023;           // highest potentiometer valye achievable
+const int NOISE_JUMP = 500;         // minimum jump in potentiometer values to define a whole turn
+
+// CONTROL CONSTANTS
+const double C = 0.005;             // see Desmos graph for intuition for C
+const double E = 2.71828;           // the natural constant e
+const double K_p = 0.7;             // proportional control constant
+const int MAX_FORWARD = 1600;       // maximum permitted forward PWM (m_f)
+const int MAX_BACKWARD = 1400;      // maximum permitted backward PWM (m_b)
+const int CHANGE_RATE = 1000;       // the interval at which new goal values are generated (milliseconds)
 
 
-Servo motor;
-axStack deriv;
+// DYNAMIC VARIABLES-----------------------------------------------------------
+Servo motor;        // motor Servo object
+int curr = 0;       // current potentiometer position
+int prev = 0;       // potentiometer position from last loop
+int turns = 0;      // number of whole turns completed by potentiometer
+int goal = 0;       // goal position, alwasy should be in [POT_ZERO, POT_MAX]
+float timer = 0;    // timer variable (in milliseconds)
 
-int curr = 0;
-int prev = 0;
-int turns = 0;
-int goal = 0;
-
-float timer = 0;
 
 void setup() {
-  // put your setup code here, to run once:
+  // Assign pins begin serial...
   Serial.begin(9600);
-  deriv.initialize(100, 0.0);
   pinMode(LED_BUILTIN, OUTPUT);
   digitalWrite(LED_BUILTIN, LOW);
   motor.attach(MOTOR_PIN);
   delay(1000);
+
+  // Calibrate ESC...LED_BUILTIN will turn on after this step
   motor.writeMicroseconds(CALIBRATION_PWM);
   delay(CALIBRATION_TIME);
+
+  // Set other variables up...
   digitalWrite(LED_BUILTIN, HIGH);
   prev = (int)analogRead(A8);
   timer = millis();
-  goal = (int)(random(0, 1023));
+  goal = (int)(random(POT_ZERO, POT_MAX));
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
-  if (millis() - timer > 1000) {
-    goal = (int)(random(0,1023));
+  // Generate value to target (random between POT_ZERO and POT_MAX)
+  if (millis() - timer > CHANGE_RATE) {
+    goal = (int)(random(POT_ZERO , POT_MAX));
     timer = millis();
   }
+
+  // Control to goal using p control
   motor.writeMicroseconds(p(goal));
+
+  // Read and interpret current pot position. Try to estimate if a whole turn has occurred
   curr = (int)analogRead(A8);
-  deriv.enqueue(curr);
-  if (curr - prev < -500) {
+  if (curr - prev < -1 * NOISE_JUMP) {
     turns++;
-  } else if (curr - prev > 500) {
+  } else if (curr - prev > NOISE_JUMP) {
     turns--;
   }
+  prev = curr;
 
-  float theta = (curr + turns * 1023) / 1023.0 * 2 * PI;
+  // Print and wait
+  print_info();
+  delay(1000.0 / CONTROL_RATE);
+}
+
+/*
+ * Prints info from global variables
+ *
+ * @params: None
+ * @return: None
+*/
+void print_info() {
   Serial.print(goal);
   Serial.print(" ");
-  //Serial.print(p(goal));
-  //Serial.print(" ");
   Serial.println(get_pos());
-  delay(1000.0 / CONTROL_RATE);
-  prev = curr;
 }
 
+
+/*
+ * P-control using safer control tactic (see Desmos plot in README)
+ *
+ * @params
+ *  - desired:  the desired potentiometer value to reach
+ * 
+ * @return:
+ *  - (int):    the PWM value to output to the ESC
+*/
 int p(int desired) {
-  //  if (get_pos() >= 1000) {
-  //    return 1500;
-  //  } else if (curr <= 23) {
-  //    return 1500;
-  //  }
   int ret = 1500 + (desired - get_pos()) * K_p;
   ret += pow(E,1 / (C * get_pos())); 
-  ret -= pow(E, 1 / (C * (1023 - get_pos())));
-  return min(1600, max(1400, ret));
+  ret -= pow(E, 1 / (C * (POT_MAX - get_pos())));
+  return min(MAX_FORWARD, max(MAX_BACKWARD, ret));
 }
 
+
+/*
+ * Gets the 'global' position of the output shaft (tries to estimate whole turns)
+ *
+ * @params:   None
+ * 
+ * @return
+ *  - (int):  the estimated global shaft position
+*/
 int get_pos() {
   int ret = curr + 1023 * turns;
   return max(0, min(1023, ret));
