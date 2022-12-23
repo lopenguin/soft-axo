eab43ce163342e407e71d7ce2924c6ddcc9ad3a2
Neil Janwani <neiljanwani@Neils-Laptop.gha.chartermi.net>
2022-12-23 17:51:28 -0500

    need to update computer

--- Gait_analysis/Data/transpose.py
+++ Gait_analysis/Data/transpose.py
@@ -13,13 +13,31 @@ FEATURES = [np.average,
 # FEATURES = [lambda x : x[0],
 #             np.average]
 
+# MEASURES = ['Header',
+#             'pelvis_tilt',
+#             'pelvis_list',
+#             'pelvis_rotation',
+#             'pelvis_tx',
+#             'pelvis_ty',
+#             'pelvis_tz',
+#             'hip_flexion_r',
+#             'hip_adduction_r',
+#             'hip_rotation_r',
+#             'knee_angle_r',
+#             'ankle_angle_r',
+#             'subtalar_angle_r',
+#             'mtp_angle_r',
+#             'hip_flexion_l',
+#             'hip_adduction_l',
+#             'hip_rotation_l',
+#             'knee_angle_l',
+#             'ankle_angle_l',
+#             'subtalar_angle_l',
+#             'mtp_angle_l']
 MEASURES = ['Header',
-            'foot_Accel_X',
-            'foot_Accel_Y',
-            'foot_Accel_Z',
-            'foot_Gyro_X',
-            'foot_Gyro_Y',
-            'foot_Gyro_Z']
+            'ankle_angle_r',
+            'subtalar_angle_r',
+            'mtp_angle_r']
 
 # MEASURES = ['Header',
 #             'foot_Accel_X',
@@ -32,7 +50,7 @@ MEASURES = ['Header',
 labels = Labeler(20, r'AB06/fsr.csv').real_write()
 # labels = np.vstack((np.linspace(0, 1, 10000), np.linspace(0, 1, 10000)))
 print(labels[0][0], labels[0][-1])
-emulator = Emulator(FEATURES, 1, labels, r'AB06/imu.csv', MEASURES, r'pdata.csv')
+emulator = Emulator(FEATURES, 1, labels, r'AB06/ik.csv', MEASURES, r'pdata.csv')
 # emulator = Emulator(FEATURES, 0.5, labels, r'func.csv', MEASURES, r'fdata.csv')
 
 
--- Gait_analysis/Neural_net/neuraxo.py
+++ Gait_analysis/Neural_net/neuraxo.py
@@ -1,60 +1,47 @@
-DATA = r'pdata.csv'
-N_FEATURES = 14
-
-LAYERS = [12, 6, 3] #[30, 50, 70, 30, 10]
-
 # first neural network with keras tutorial
 from numpy import loadtxt
 from tensorflow.keras.models import Sequential
 from tensorflow.keras.layers import Dense, Add
 from tensorflow.keras.layers import Layer
 
+DATA = r'pdata.csv'
+N_FEATURES = 21
+EPOCHS = 30 #3
+BATCH_SIZE = 15 #5
+ACTIVATION = 'sigmoid'
+TRAIN = 20000
+
+LAYERS = [21, 10, 10, 10, 5] #[100, 50, 25, 25, 12] 
 
 # load the dataset
 dataset = loadtxt(DATA, delimiter=',')
-print(dataset)
-print('askdflkjsaldf')
 # split into input (X) and output (y) variables
 X = dataset[:, 1:N_FEATURES + 1] # CURRENTLY INPUTTING TRIVIAL PROBLEM DATA.....
 y = dataset[:,N_FEATURES + 1]
-print(X)
 # define the keras model
 model = Sequential()
-model.add(Dense(LAYERS[0], input_shape=(N_FEATURES,), activation='relu'))
+model.add(Dense(LAYERS[0], input_shape=(N_FEATURES,), activation=ACTIVATION))
 for i in LAYERS[1:]:
-    model.add(Dense(i, activation='relu'))
+    model.add(Dense(i, activation=ACTIVATION))
 # model.add(Add())
-model.add(Dense(1, activation='sigmoid'))
+model.add(Dense(1, activation=ACTIVATION))
 # compile the keras model
 model.compile(loss='binary_crossentropy', optimizer='adam', metrics=['accuracy'])
 # fit the keras model on the dataset
-TRAIN = 20000
-model.fit(X[:TRAIN], y[:TRAIN], epochs=3, batch_size=5)
-# evaluate the keras model
-# _, accuracy = model.evaluate(X, y)
-# print('Accuracy: %.2f' % (accuracy*100))
-print(len(X))
+
+model.fit(X[:TRAIN], y[:TRAIN], epochs=EPOCHS, batch_size=BATCH_SIZE)
+
 error = 0
 tols = [0.01, 0.05, 0.075, 0.1, 0.20]
 wrong = [0] * len(tols)
 y_pred = model.predict(X[TRAIN:])
 for i, yp in enumerate(y_pred):
-    error += abs(y[TRAIN + i] - yp)
+    error += min(abs(y[TRAIN + i] - yp), 1 - abs(y[TRAIN + i] - yp))
     for k, t in enumerate(tols):
-        if abs(y[TRAIN + i] - yp) >= t:
+        if min(abs(y[TRAIN + i] - yp), 1 - abs(yp - y[TRAIN + i])) >= t:
             wrong[k] += 1
 
-# print(y[:200], y_pred[:200])
-print('Off by (avg):',error**0.5 / len(y_pred))
+print('Off by (avg):',error / len(y_pred))
 for w, t in zip(wrong, tols):
     print('NN is within ' + str(t * 100) + r'% of the time', 1 - w / len(y_pred))
-# input1 = tf.keras.layers.Input(shape=(16,))
-# x1 = tf.keras.layers.Dense(8, activation='relu')(input1)
-# input2 = tf.keras.layers.Input(shape=(32,))
-# x2 = tf.keras.layers.Dense(8, activation='relu')(input2)
-# added = tf.keras.layers.arcTan([x1, x2]) ?????
-# out = tf.keras.layers.Dense(1)(added)
-# model = tf.keras.models.Model(inputs=[input1, input2], outputs=out)
 
-# can we just do model.add(arcTan())?
-# checkout the lambda layer (you can specify your own function...)
