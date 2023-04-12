584a3f8da51afb5907356b5cceb3e196ae390d24
Neil Janwani <neiljanwani@Neils-Laptop.local>
2023-04-12 08:39:15 -0700

    forgot to add something for FOC/BLE update

---
+++ Arduino/BLE_example/BLE_example.ino
@@ -0,0 +1,81 @@
+#include <Arduino.h>
+#include "Adafruit_BLE.h"
+#include "Adafruit_BluefruitLE_UART.h"
+#include "BluefruitConfig.h"
+
+#define FACTORYRESET_ENABLE         0
+#define MINIMUM_FIRMWARE_VERSION    "0.6.6"
+
+Adafruit_BluefruitLE_UART ble(BLUEFRUIT_HWSERIAL_NAME, BLUEFRUIT_UART_MODE_PIN);
+
+long i = 0;
+
+void error(const __FlashStringHelper*err) {
+  Serial.println(err);
+  while (1);
+}
+
+void setup(void)
+{
+  Serial.begin(115200);
+  Serial.println(F("Adafruit Bluefruit AXO"));
+  Serial.println(F("---------------------------------------"));
+
+  /* Initialise the module */
+  Serial.print(F("Initialising the Bluefruit LE module: "));
+
+  if ( !ble.begin(VERBOSE_MODE) )
+  {
+    error(F("ERROR: Couldn't find Bluefruit, make sure it's in CoMmanD mode & check wiring."));
+  }
+  Serial.println( F("OK!") );
+
+  if ( FACTORYRESET_ENABLE )
+  {
+    /* Perform a factory reset to make sure everything is in a known state */
+    Serial.println(F("Performing a factory reset: "));
+    if ( ! ble.factoryReset() ) {
+      error(F("ERROR: Couldn't factory reset."));
+    }
+  }
+
+  /* Disable command echo from Bluefruit */
+  ble.echo(false);
+
+  Serial.println("Requesting Bluefruit info:");
+  /* Print Bluefruit information */
+  ble.info();
+  ble.setMode(BLUEFRUIT_MODE_DATA);
+
+  /* Change the device name to make it easier to find */
+  Serial.println(F("Setting device name to 'AXO something': "));
+  ble.sendCommandCheckOK(F( "AT+GAPDEVNAME=AXO-LEFT" ));
+  ble.sendCommandCheckOK(F( "AT+BleHIDEn=On" ));
+  ble.sendCommandCheckOK(F( "AT+BleKeyboardEn=On"  ));
+
+  if (! ble.sendCommandCheckOK(F( "AT+GAPDEVNAME=AXO" )) ) {
+    error(F("ERROR: Could not set device name."));
+  }
+
+  /* Add or remove service requires a reset */
+  Serial.println(F("Performing a SW reset."));
+  if (! ble.reset() ) {
+    error(F("ERROR: Couldn't reset."));
+  }
+
+  Serial.println("Success! Starting...");
+  delay(15000);
+}
+
+void loop(void) {
+  //  char ret;
+  //  if (Serial.available()) {
+  //    ret = Serial.read();
+  //    Serial.write(ret);
+  //    ble.write(ret);
+  //  }
+  if (i < 20000) {
+      ble.write((char)(97 + i % 26));
+  }
+  i++;
+}
